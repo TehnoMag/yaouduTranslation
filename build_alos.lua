72524e34781bcf0c3e4029e0bcad48ac0b8589ca
Monarch8686 <114637789+Monarch8686@users.noreply.github.com>
2023-08-06 03:49:39 -0800

    Alos Ships names in a skirmish update
    * Update build_alos.lua
    
    * Update build_alos.lua
    
    * Update research_alos.lua
    
    Fix rebase

--- build_alos.lua
+++ build_alos.lua
@@ -7,9 +7,9 @@ Locale_Text[10] = "Bomber Hangar"
 Locale_Text[11] = "Deploys 4 squadrons of Bombers for Anti-Ship Operations"
 
 
-Locale_Text[50] = "Fighter"
-Locale_Text[51] = "Standard Fighter, used for Anti-Aircraft combat.\n<b>Prerequisite:</b> Weapon Tech 1\n<b>Role:</b> Defensive Unit"
-Locale_Text[60] = "Bomber"
+Locale_Text[50] = "Viper MK.I"
+Locale_Text[51] = "Standard fighter, used for Anti-Aircraft combat.\n<b>Prerequisite:</b> Weapon Tech 1\n<b>Role:</b> Defensive Unit"
+Locale_Text[60] = "Viper MK.II"
 Locale_Text[61] = "Standard Bomber, used for Anti-Ship combat.\n<b>Prerequisite:</b> Weapon Tech 1\n <b>Role:</b> Offensive Unit"
 
 Locale_Text[100] = "Resource Collector"
@@ -22,21 +22,21 @@ Locale_Text[110] = "Anti-Aircraft Frigate"
 Locale_Text[111] = "Anti-Aircraft Frigate.\n<b>Prerequisite:</b>  Weapon Tech 1, Hull Tech 1\n<b>Role:</b> Defensive Unit"
 Locale_Text[115] = "Ion Beam Frigate" 
 Locale_Text[116] = "Anti-Ship Frigate, Capable of effectively attacking larger ship classes in numbers.\n<b>Prerequisite:</b> Weapon Tech 1, Hull Tech 1\n<b>Role:</b> Offensive Unit"
-Locale_Text[120] = "Plasma Assault Ship"
+Locale_Text[120] = "Plasma Assault Frigate"
 Locale_Text[121] = "Assault Frigate armed with a plasma cannon.\n<b>Prerequisite:</b>Weapon Tech 1, Hull Tech 1\n<b>Role:</b> Offensive Unit"
-Locale_Text[125] = "Anti-Ship Torpedo Assault Ship"
+Locale_Text[125] = "Torpedo Assault Frigate"
 Locale_Text[126] = "Assault Ship equipped with Torpedoes, Multi-role.\n<b>Prerequisite:</b> Weapon Tech 2, Hull Tech 1\n<b>Role:</b> Defensive Unit"
 
 
-Locale_Text[150] = "Heavy Missile Boat"
-Locale_Text[151] = "Heavy Missile Escort boats, suitable for cluster sorties to deal heavy blows to the enemy.\n<b>Prerequisite:</b> Weapon Tech 2, Hull Tech 2\n<b>Role:</b> Defensive Unit"
+Locale_Text[150] = "Missile Corvette"
+Locale_Text[151] = "Missile Escort Corvettes, suitable for cluster sorties to deal heavy blows to the enemy.\n<b>Prerequisite:</b> Weapon Tech 2, Hull Tech 2\n<b>Role:</b> Defensive Unit"
 
 
 
 Locale_Text[200] = "Ion Beam Destroyer"
 Locale_Text[201] = "Destroyer armed with a Heavy Ion Beam, Capable of effectively attacking larger ship classes in numbers.\n<b>Prerequisite:</b> Weapon Tech 3, Hull Tech 3\n<b>Role:</b> Offensive Unit"
 
-Locale_Text[210] = "Artillery Destroyer"
+Locale_Text[210] = "Cannon Destroyer"
 Locale_Text[211] = "Destroyer armed with large caliber guns. Low cost, Suitable for suppressing the attack of enemy frigates groups.\n<b>Prerequisite:</b>2 Weapon Tech 2, Hull Tech 2\n<b>Role:</b> Defensive Unit"
 Locale_Text[215] = "Anti-Air Destroyer"
 Locale_Text[216] = "Destroyer armed with Anti-Aircraft guns.\n<b>Prerequisite:</b>Weapon Tech 2, Hull Tech 2\n<b>Role:</b> Defensive Unit"
@@ -50,27 +50,27 @@ Locale_Text[251] = "Auxiliary carrier, Produces fighters and can call in larger 
 Locale_Text[255] = "Detection System"
 Locale_Text[256] = "Advanced Detection System\n<b>Prerequisite:</b> Weapon Tech 2\n<b>Role:</b> Subsystem"
 
-Locale_Text[260] = "Beam Sniper Ship"
+Locale_Text[260] = "Nabehilles Cruiser"
 Locale_Text[261] = "Cruiser class Ship designed to attack large enemy units at long range.\n<b>Prerequisite:</b>Weapon Tech 3, Hull Tech 3/n<b>Role:</b> Offensive Unit"
 
-Locale_Text[270] = "Defense Cruiser"
+Locale_Text[270] = "Femilins Cruiser"
 Locale_Text[271] = "Support cruiser with heavy armor and defensive shields meant to suppress large groups of smaller warships.\n<b>Prerequisite:</b>Weapon Tech 3, Hull Tech 3.\n<b>Role:</b> Defensive Unit"
 
-Locale_Text[300] = "Battlecruiser - Femilynes"
+Locale_Text[300] = "Femilins Battlecruiser"
 Locale_Text[301] = "Battlecruiser equipped with large caliber guns. Performance close to a Battleships, in comparison to Battlecruiser-Naberius, has a higher speed and range, slower fire rate.\n<b>Prerequisite:</b> Weapon Tech 3, Hull Tech 4\n<b>Role:</b> Defensive Unit"
 
 
-Locale_Text[305] = "Battlecruiser - Naberius"
+Locale_Text[305] = "Abelian Battlecruiser"
 Locale_Text[306] = "Battlecruiser equipped with large ion cannon, Performance close to a Battleships, compared to Battlecruiser - Femilynes, higher accuracy and power , Smaller range, suitable for attacking single targets.\n<b>Prerequisite:</b> Weapon Tech 4, Hull Tech 4\n<b>Role:</b> Offensive Unit"
 
 
-Locale_Text[400] = "Battleship - Talos"
+Locale_Text[400] = "Talos Class Battleship (Cannon)"
 Locale_Text[401] = "Defensive Battleship equipped with a large number of heavy guns. Strong offensive and defensive capabilities.\n<b>Prerequisite:</b>Weapon Tech 4, Hull Techn 4\n<b>Role:</b> Defensive Unit"
 
 
-Locale_Text[405] = "Battleship - Talos Beam Refit"
+Locale_Text[405] = "Talos Class Battleship (Beam)"
 Locale_Text[406] = "Talos class with Ion turrets, designed to suppress the enemy.\n<b>Prerequisite:</b> Weapon Tech 4, Hull Tech 4\n<b>Role:</b> Defensive Unit"
-Locale_Text[407] = "Battleship - Indras"
+Locale_Text[407] = "Indellas Class Battleship"
 Locale_Text[408] = "Electromagnetic Gun Battleship, designed to suppress the enemy.\n<b>Prerequisite:</b> Weapon Tech 5, Hull Tech 4\n<b>Role:</b> Defensive Unit"
 
 
@@ -83,7 +83,7 @@ Locale_Text[450] = "Vampire Super Fighter"
 Locale_Text[451] = "Hi-Tech Fighter with heavy armor and powerful weapons.\n<b>Prerequisite:</b> Weapon Tech 4, Hull Tech 4\n<b>Role:</b> Offensive Unit"
 
 
-Locale_Text[455] = "Fortress Ship - Safrielle"
+Locale_Text[455] = "Siflier Fortress Ship"
 Locale_Text[456] = "The most powerful ship of the Alos Empire, Unimaginable firepower and combat power.\n<b>Prerequisite:</b> Weapon Tech 6,Hull Tech 6\n<b>Role:</b> Ultimate Weapon"
 
 
@@ -102,36 +102,36 @@ Locale_Text[521] = "Platform equipped with Area of Effect Pulse Cannon, Moves sl
 
 Locale_Text[530] = "Gravity Field Generator"
 Locale_Text[531] = "Advanced gravity field generator, interferes with Hyperspace function of the enemy.\n<b>Prerequisite:</b> Hull Tech 2"
-Locale_Text[540] = "Greenbluster Fortress"
+Locale_Text[540] = "Green Brewster Fortress"
 Locale_Text[541] = "An immobile fortress. Can be redployed with Hyperspace Jump.\n<b>Prerequisite:\b> Weapon Tech 3, Hull Tech 5"
 
 
 
 
-Locale_Text[550] = "ADF-01 Fighter"
-Locale_Text[560] = "ADF-02 Fighter"
-Locale_Text[570] = "CFA-44 Fighter"
-Locale_Text[580] = "MIG33 Black Fighter"
-Locale_Text[590] = "Su-33 Strigon Fighter"
+Locale_Text[550] = "ADF-01 Falken"
+Locale_Text[560] = "ADF-02 Drache"
+Locale_Text[570] = "CFA-44 Nosferatu"
+Locale_Text[580] = "MIG-33 Super Fulcrum"
+Locale_Text[590] = "Su-33 Strigon"
 Locale_Text[600] = "Viper MkI Fighter"
-Locale_Text[610] = "XFA-27 Valkyrie Fighter"
+Locale_Text[610] = "XFA-27 Scarface"
 Locale_Text[615] = "Alos Empire's Elite Fighter"
 
 
-Locale_Text[620] = "Plasma Cannon Assault Craft"
+Locale_Text[620] = "Plasma Assault Corvette"
 Locale_Text[625] = "Assault craft armed with plasma weapons.\n<b>Prerequisite:</b> Weapon Tec 2,Hull Tech 2"
 
-Locale_Text[630] = "Peregrine Falcon"
+Locale_Text[630] = "Falcon Aviation Battleship"
 Locale_Text[635] = "A new type of warship from the Vaygr.\n<b>Prerequisite:</b> Weapon Tech 3,  Hull Tech 4\n<b>Hero ship:</b> limited hero ship, appearing with ACE fighter squad"
-Locale_Text[640] = "Cruise control ship"
+Locale_Text[640] = "Silmeria Carrier"
 Locale_Text[645] = "Experimental carrier with heavy armor and missile arrays.\n<b>Prerequisite:</b> Weapon Tech 3, Hull Tech 5\n<b>Hero ship:</b> limited hero ship, appearing with ACE fighter squadron"
-Locale_Text[650] = "Grace Marea"
+Locale_Text[650] = "Gracemeria Carrier"
 Locale_Text[655] = "Experimental carrier with high movement speed.\n<b>Prerequisite:</b> Weapon Tech 3, Hull Tech 5\n<b>Hero ship:</b> limited hero ship, appearing with ACE fighter squadron"
-Locale_Text[660] = "Thermaltake battleship"
+Locale_Text[660] = "Yaofeng Battleship"
 Locale_Text[665] = "Special battleship customized by foreign civilization envoy, with powerful siege main gun and strong firepower.\n<b>Prerequisite:</b> Weapon Tech 5, Hull Tech 4\n<b>Hero ship:</b> limited hero ship, appearing with ACE fighter squad"
 
 
-Locale_Text[700] = "Space Ripper Class Cruiser"
+Locale_Text[700] = "Space Raider Class Cruiser"
 Locale_Text[701] = "Alos' newly developed main cruiser, with strong armor and good speed.\n<b>Prerequisite:</b> Weapon Tech 2, Hull Tech 3"
 
 
@@ -139,7 +139,7 @@ Locale_Text[710] = "Alberdeo Super Battleship"
 Locale_Text[711] = "The latest super battleship developed by Alos, with powerful armament and floating cannon group.\n<b>Prerequisite:</b> Weapon Tech 5, Hull Tech 5"
 
 
-Locale_Text[720] = "Battlecruiser - Heimdall"
+Locale_Text[720] = "Heimdall Battlecruiser"
 Locale_Text[721] = "The latest Battlecruiser developed by Alos, with battleship-class main guns and a large number of large-caliber main guns.\n<b>Prerequisite:</b> Weapon Tech 4, Hull Tech 4"
 
 
@@ -157,5 +157,5 @@ Locale_Text[1031] = "Fai'ri Empire-aided battlefield engineering ship, capable o
 Locale_Text[1040] = "Black Cat-class engineering ship"
 Locale_Text[1041] = "Large engineering ship assisted by the Fai'ri Empire, with powerful repair capabilities"
 
-Locale_Text[1050] = "judgment day"
+Locale_Text[1050] = "Judgement Day"
 Locale_Text[1051] = "The Judgment Day of the Rhine Sauron Legion of the Alos Empire is the last hope for the empire"
