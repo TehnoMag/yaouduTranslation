b790df2569c67fcf6a7aeb7049f7f838a2fdb09f
Михаил Лукьянов <delta-server@ya.ru>
2026-02-09 13:24:58 +0300

    factory restored

--- factory.lua
+++ factory.lua
@@ -1,76 +1,78 @@
 Factory_Text = {}
-Factory_Text[1000] = "纯净水提炼装置"            --1
-Factory_Text[1001] = "可以将冰矿融化成淡水，每分钟消耗1单位的冰矿和1单位的核燃料，生产出20单位的水"
-Factory_Text[1005] = "啤酒酿造厂"                --2
-Factory_Text[1006] = "可以用小麦酿造啤酒，每分钟消耗5单位的小麦和5单位的淡水，生产出5单位的啤酒"
+Factory_Text[1000] = "Pure water refining device" --1
+Factory_Text[1001] = "Can melt ice ore into fresh water, consuming 1 unit of ice ore and 1 unit of nuclear fuel per minute to produce 20 units of water"
+Factory_Text[1005] = "beer brewery" --2
+Factory_Text[1006] = "You can brew beer from wheat, consuming 5 units of wheat and 5 units of fresh water per minute to produce 5 units of beer"
 ---------------------------------------------------
-Factory_Text[1100] = "氢燃料提炼装置"            --3
-Factory_Text[1101] = "可以高效率的将淡水电解出氢燃料，每分钟消耗10单位的水，生产出2单位的氢燃料"
-Factory_Text[1105] = "核燃料提炼装置"            --4
-Factory_Text[1106] = "从一些珍稀矿石中提炼出核燃料，每分钟消耗1单位的珍稀矿石，生产出20单位的核燃料"
-Factory_Text[1110] = "反物质燃料提炼装置"        --5 
-Factory_Text[1111] = "将大量的能源和一些珍稀元素一起压缩成反物质燃料，每分钟消耗20单位的核燃料和1单位的珍稀矿石，生产出10单位的反物质燃料"
+Factory_Text[1100] = "Hydrogen fuel refining device" --3
+Factory_Text[1101] = "It can electrolyze fresh water to produce hydrogen fuel with high efficiency, consuming 10 units of water per minute to produce 2 units of hydrogen fuel"
+Factory_Text[1105] = "Nuclear fuel refinery" --4
+Factory_Text[1106] = "Refining nuclear fuel from some rare ores, consuming 1 unit of rare ore per minute to produce 20 units of nuclear fuel"
+Factory_Text[1110] = "Antimatter fuel refining device" --5 
+Factory_Text[1111] = "Compress a large amount of energy together with some rare elements into antimatter fuel, consuming 20 units of nuclear fuel and 1 unit of rare ore per minute to produce 10 units of antimatter fuel"
 ---------------------------------------------------
-Factory_Text[1200] = "面包烘焙坊"                --6
-Factory_Text[1201] = "将小麦研磨成面粉后高效率的烤制成面包。每分钟消耗20单位的小麦，生产出10单位的面包"
-Factory_Text[1205] = "快餐食品店"                --7
-Factory_Text[1206] = "将面包和猪肉一起烹制成汉堡三明治等美食。每分钟消耗20单位的面包和10单位的猪肉，生产出20单位的三明治"
-Factory_Text[1210] = "高级厨房"                  --8 
-Factory_Text[1211] = "将牛肉经过精心的调理，制作成美味的牛排。每分钟消耗10单位牛肉，生产出2单位的太空牛排"
+Factory_Text[1200] = "Bread bakery" --6
+Factory_Text[1201] = "Grind wheat into flour and bake it into bread with high efficiency. Consume 20 units of wheat per minute to produce 10 units of bread"
+Factory_Text[1205] = "Fast food restaurant" --7
+Factory_Text[1206] = "Cooks bread and pork together to make burger sandwiches and other delicacies. Consume 20 units of bread and 10 units of pork per minute to produce 20 units of sandwiches"
+Factory_Text[1210] = "Advanced Kitchen" --8 
+Factory_Text[1211] = "Season beef carefully to make delicious steaks. Consume 10 units of beef per minute to produce 2 units of space steak"
 
-Factory_Text[1300] = "日用品加工模块"            --9 
-Factory_Text[1301] = "将一些基础合金制作成简单的生活用品。每分钟消耗1位的基础合金，生产出10单位的日常生活用品"
+Factory_Text[1300] = "Daily processing module" --9 
+Factory_Text[1301] = "Craft some basic alloys into simple household items. Consume 1 bit of basic alloy per minute to produce 10 units of daily necessities"
 
-Factory_Text[1400] = "谷物农场"                  --10
-Factory_Text[1401] = "用淡水和能源高效率的培育出谷物来。每分钟消耗10单位的淡水和1单位的核燃料，生产出30单位的谷物"
-Factory_Text[1405] = "生猪饲养场"                --11
-Factory_Text[1406] = "用淡水和谷物饲养生猪，然后屠宰后获得大量猪肉。每分钟消耗5单位的淡水和10单位的谷物，生产出5单位的猪肉"
-Factory_Text[1410] = "奶牛饲养场"                --12
-Factory_Text[1411] = "用淡水和谷物饲养奶牛，然后屠宰后获得大量牛肉。每分钟消耗10单位的淡水和10单位的谷物，生产出3单位的牛肉"
+Factory_Text[1400] = "Grain farm" --10
+Factory_Text[1401] = "Cultivate grains efficiently with fresh water and energy. Every minute 10 units of fresh water and 1 unit of nuclear fuel are consumed to produce 30 units of grain"
+Factory_Text[1405] = "Pig farm" --11
+Factory_Text[1406] = "Raise pigs with fresh water and grains, then slaughter them to obtain large amounts of pork. Every minute 5 units of fresh water and 10 units of grain are consumed to produce 5 units of pork"
+Factory_Text[1410] = "Dairy farm" --12
+Factory_Text[1411] = "Raise cows with fresh water and grains, then slaughter them to obtain large amounts of beef. Every minute 10 units of fresh water and 10 units of grain are consumed to produce 3 units of beef"
 
-Factory_Text[1500] = "基础工具制作车间"          --13
-Factory_Text[1501] = "用各种金属生产出最基础的维护工具。每分钟消耗10单位的基础合金和5单位的高强度合金，生产出5单位的基础工具"
-Factory_Text[1505] = "高科技加工设备间"          --14
-Factory_Text[1506] = "在基础工具的基础上制作出高性能的维护工具。每分钟消耗10单位的基础工具和5单位的高强度合金，生产出3单位的高级工具"
-Factory_Text[1510] = "自动机器人生产线"          --15      
-Factory_Text[1511] = "用大量金属材料制作出昂贵的自动维护机器人。每分钟消耗2单位的高级工具和10单位的高强度合金，生产出2单位的自动维护机器人"
+Factory_Text[1500] = "Basic tool making workshop" --13
+Factory_Text[1501] = "Produces the most basic maintenance tools from various metals. Produce 5 units of basic tools by consuming 10 units of basic alloy and 5 units of high strength alloy every minute"
+Factory_Text[1505] = "High-tech processing equipment room" --14
+Factory_Text[1506] = "Produce high performance maintenance tools based on basic tools. Consume 10 units of basic tools and 5 units of high strength alloys per minute to produce 3 units of high level tools"
+Factory_Text[1510] = "Automatic robot production line" --15      
+Factory_Text[1511] = "Produces expensive automatic maintenance robots from large amounts of metal. Consume 2 units of advanced tools and 10 units of high-strength alloys per minute to produce 2 units of automated maintenance robots"
 
-Factory_Text[1600] = "基础合金冶炼熔炉"          --16
-Factory_Text[1601] = "用高温将金属矿石中的金属元素提炼出来，制造成合金。每分钟消耗1单位的金属矿石和1单位的核燃料，生产出10单位的基础合金"
-Factory_Text[1605] = "高强度合金冶炼设备"        --17
-Factory_Text[1606] = "在基础合金上添加一些珍稀矿物增加强度，从而生产出具有更高性能的高强度合金。每分钟消耗10单位的基础合金和1单位的核燃料，还有1单位的珍稀矿石，生产出5单位的高强度合金"
-Factory_Text[1610] = "高温熔炉"                  --18
-Factory_Text[1611] = "用高性能的锻造熔炉和大量能量直接将基础合金升级为高强度合金。每分钟消耗10单位的基础合金和3单位的核燃料，生产出10单位的高强度合金"
-Factory_Text[1615] = "特种合金冶炼设备"          --19
-Factory_Text[1616] = "在高强度合金的基础上添加特殊元素，将其升级为特种合金。每分钟消耗10单位的高强度合金和5单位的珍稀矿石1单位的反物质燃料，生产出5单位的特种合金"
+Factory_Text[1600] = "Basic alloy smelting furnace" --16
+Factory_Text[1601] = "Refines metal elements from metal ores with high temperature to create alloys. Consumes 1 unit of metal ore and 1 unit of nuclear fuel per minute to produce 10 units of base alloy"
+Factory_Text[1605] = "High-strength alloy smelting equipment" --17
+Factory_Text[1606] = "Add some rare minerals to the base alloy to increase its strength, thus producing a high strength alloy with higher performance. Consume 10 units of base alloy and 1 unit of nuclear fuel and 1 unit of rare ore per minute to produce 5 units of high strength alloy"
+Factory_Text[1610] = "High temperature furnace" --18
+Factory_Text[1611] = "Upgrade base alloys directly to high strength alloys with high performance forging furnaces and large amounts of energy. Consume 10 units of base alloy and 3 units of nuclear fuel per minute to produce 10 units of high strength alloy"
+Factory_Text[1615] = "Special alloy smelting equipment" --19
+Factory_Text[1616] = "Add special elements to high-strength alloys to upgrade them to special alloys. Every minute consume 10 units of high-strength alloy and 5 units of rare ore 1 unit of antimatter fuel to produce 5 units of special alloy"
 
-Factory_Text[1700] = "小口径武器配件加工流水线"  --20
-Factory_Text[1701] = "在生产小口径武器所必须的各种零配件。每分钟消耗10单位的基础合金和2单位的高强度合金，生产出5单位的小口径武器配件"
-Factory_Text[1705] = "中型武器配件加工车间"      --21
-Factory_Text[1706] = "在生产小口径武器所必须的各种零配件。每分钟消耗20单位的基础合金和10单位的高强度合金，生产出5单位的中型武器配件"
-Factory_Text[1710] = "大型武器配件加工厂"        --22
-Factory_Text[1711] = "在生产小口径武器所必须的各种零配件。每分钟消耗10单位的高强度合金和2单位的特种合金，生产出5单位的大型武器配件"
+Factory_Text[1700] = "Small caliber weapon parts processing line" --20
+Factory_Text[1701] = "In the production of various spare parts necessary for small-caliber weapons. Every minute 10 units of basic alloy and 2 units of high strength alloy are consumed to produce 5 units of small caliber weapon parts"
+Factory_Text[1705] = "Medium weapon parts processing workshop" --21
+Factory_Text[1706] = "In the production of various spare parts necessary for small-caliber weapons. Every minute 20 units of basic alloy and 10 units of high strength alloy are consumed to produce 5 units of medium weapon parts"
+Factory_Text[1710] = "Large weapon parts processing plant" --22
+Factory_Text[1711] = "In the production of various spare parts necessary for small caliber weapons. Every minute 10 units of high-strength alloys and 2 units of special alloys are consumed to produce 5 units of large weapon parts"
 
-Factory_Text[1800] = "超空间模块"        --
-Factory_Text[1801] = "消耗反物质能量在航母周围生成一个大范围的跳跃立场，引导友军舰队进行跳跃。每分钟消耗1单位的反物质能量"
+Factory_Text[1800] = "Hyperspace module" --
+Factory_Text[1801] = "Consumes antimatter energy to generate a large jump stance around the carrier to guide friendly fleets through the jump. Consumes 1 unit of antimatter energy per minute"
 
-Factory_Text[1850] = "高效能废水处理设备"        --23
-Factory_Text[1851] = "一个高性能的废水处理设施，可以更高效的循环用水，从而节约出更多淡水给其他设备使用。每分钟产出10单位的淡水"
-Factory_Text[1860] = "生活垃圾处理设施"        --24
-Factory_Text[1861] = "一种高效能的生活垃圾处理设备，可以将其分解成土壤和肥料，种植农作物。每分钟消耗10单位生活垃圾，产出3单位的谷物"
+Factory_Text[1850] = "High-efficiency wastewater treatment facility" --23
+Factory_Text[1851] = "A high performance wastewater treatment facility that recycles water more efficiently, thus saving more fresh water for other equipment. Produces 10 units of fresh water per minute"
+Factory_Text[1860] = "Domestic waste treatment facility" --24
+Factory_Text[1861] = "A high-efficiency household waste treatment facility that can be broken down into soil and fertilizer to grow crops. It consumes 10 units of household waste per minute and produces 3 units of grain"
 
-Factory_Text[1865] = "基础补给合成设备"        --25
-Factory_Text[1866] = "使用基础的食材合成基础补给。每分钟消耗10单位的三明治和10单位的淡水，产出5单位的基础补给"
-Factory_Text[1870] = "弹药加工设备"        --26
-Factory_Text[1871] = "可以加工各种口径的弹药和能量武器专用的弹药包。每分钟消耗10单位的基础合金和5单位的核燃料，产出5单位的弹药"
-Factory_Text[1875] = "修理材料生产设备"        --27
-Factory_Text[1876] = "使用各种金属原材料来生产修补材料。每分钟消耗10单位的基础合金和2单位的高强度合金，产出5单位的修理材料"
+Factory_Text[1865] = "Basic supply synthesis equipment" --25
+Factory_Text[1866] = "Synthesizes basic supplies using basic ingredients. Consume 10 units of sandwiches and 10 units of fresh water per minute to produce 5 units of basic supplies"
+Factory_Text[1870] = "Ammo processing equipment" --26
+Factory_Text[1871] = "Can process ammunition of various calibers and ammunition packs for energy weapons. Every minute consumes 10 units of base alloy and 5 units of nuclear fuel to produce 5 units of ammunition"
+Factory_Text[1875] = "Repair material production equipment" --27
+Factory_Text[1876] = "Uses various metal raw materials to produce repair materials. Every minute consumes 10 units of basic alloy and 2 units of high strength alloy to produce 5 units of repair material"
 
-Factory_Text[1880] = "高级补给品生产车间"        --28
-Factory_Text[1881] = "使用一些高级食材来制作基础补给。每分钟消耗10单位的啤酒和10单位的太空牛排，产出50单位的基础补给"
-Factory_Text[1885] = "弹药加工流水线"        --29
-Factory_Text[1886] = "高效率生产弹药的流水线。每分钟消耗20单位的高强度合金和10单位的核燃料，产出50单位的弹药"
-Factory_Text[1890] = "修理材料生产工厂"        --30
-Factory_Text[1891] = "高效率的生产修补材料的自动工厂。每分钟消耗10单位的高强度合金和1单位的自动维修机器人，产出50单位的修理材料"
-Factory_Text[1895] = "人形电脑装配厂"        --31
-Factory_Text[1896] = "生产菲雅利人最热衷的人形电脑，据说一台人形电脑现在在菲雅利可以卖到数万以上。"
+Factory_Text[1880] = "Advanced supply production workshop" --28
+Factory_Text[1881] = "Use some advanced ingredients to make basic supplies. Consume 10 units of beer and 10 units of space steak every minute to produce 50 units of basic supplies"
+Factory_Text[1885] = "Ammo processing line" --29
+Factory_Text[1886] = "An assembly line for efficient ammunition production. Consumes 20 units of high-strength alloy and 10 units of nuclear fuel per minute, producing 50 units of ammunition"
+Factory_Text[1890] = "Repair material production plant" --30
+Factory_Text[1891] = "Highly efficient automated factory for producing repair materials. Consumes 10 units of high-strength alloy and 1 unit of automated repair bots per minute, producing 50 units of repair material"
+Factory_Text[1895] = "Humanoid computer assembly plant" --31
+Factory_Text[1896] = "Produces the most popular humanoid computer among Fai'rians, and it is said that a humanoid computer can now be sold for tens of thousands of dollars or more in Fai'ri."
+
+
